79a60da955f291a2345c015f9314f462c2dfcf4d
Ghulam Mustafa <50313622+mustafaboss@users.noreply.github.com>
2020-11-30 20:42:19 -0800

    university work
    my c++ perogram

---
+++ main.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    /*cout<<"hello world "<<endl;
+    cout<<"Ghulam mustafa"<<endl;
+    int a, b, c;
+    short sa=9;
+    cout<<sa;
+    return 0;*/
+    //camel case notation
+    int baberAzamScore = 83;
+    cout<<" The baber azam score in final " <<baberAzamScore;
+    float baberAzamAvarage = 65.5;
+    cout<<" baber azam score againist lahore qalander " <<baberAzamAvarage;
+}
+
---
+++ second.cpp
@@ -0,0 +1,34 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    /*cout <<"welcome to 24/7 teacher online" << endl;
+    return 0;*/
+    int A;
+    int B; 
+    int C; 
+    int D;
+    int E; 
+    int F;
+
+    A=12;
+    B=4; 
+    C = A+B;
+    D = A-B;
+    E = A*B;
+    F = A/B;
+
+    cout<<"the result of addtition is = ";
+    cout << C;
+    cout<<endl;
+    cout<<"the result of SUBTRACTION is ="; 
+    cout<<D;
+    cout<<endl;
+    cout<<"the result of MULTIPLICATION is ="; 
+    cout<<E;
+    cout<<endl;
+    cout<<"the result of DIVISION is ="; 
+    cout<<F;
+    cout<<endl;
+    cout<<"hassan khan puri dhola ";
+}
